4aa9afaa17287904446139325577d0eb4ef3fa6b
jdalton <john.david.dalton@gmail.com>
2010-08-23 17:15:22 -0400

    fusebox: Ensure Date objects are handled correctly when passed to the fusebox Object function. [jddalton]

--- fusebox.js
+++ fusebox.js
@@ -340,7 +340,7 @@
         case '[object String]' :
           classOf = classOf.slice(8,-1);
           if (value.constructor != instance[classOf]) {
-            return instance[classOf](value);
+            return new instance[classOf](value);
           }
       }
       return value;
@@ -617,7 +617,7 @@
 
     if (isFunction(datePlugin.toJSON)) {
       (datePlugin.toJSON = function toJSON() {
-        return instance.String(__toJSON.call(this));
+        return instance.String(__toJSON.call(new window.Date(this)));
       }).raw = __toJSON;
     }
 
